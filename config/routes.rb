b0d050e2949ef6f354607d757b3797f10d01e390
Clay Shaw <Claydshaw@gmail.com>
2018-03-29 16:53:33 -0700

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,51 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'group/index'
+  end
+
+  namespace :api do
+    get 'group/create'
+  end
+
+  namespace :api do
+    get 'group/destroy'
+  end
+
+  namespace :api do
+    get 'company/create'
+  end
+
+  namespace :api do
+    get 'company/destroy'
+  end
+
+  namespace :api do
+    get 'employee/create'
+  end
+
+  namespace :api do
+    get 'employee/destroy'
+  end
+
+  namespace :api do
+    get 'sessions/create'
+  end
+
+  namespace :api do
+    get 'sessions/destroy'
+  end
+
+  namespace :api do
+    get 'users/index'
+  end
+
+  namespace :api do
+    get 'users/create'
+  end
+
+  namespace :api do
+    get 'users/show'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

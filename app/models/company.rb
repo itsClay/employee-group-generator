9d03dc73305aeeefbee3c4a9acdbab02ce1a29e9
Clay Shaw <Claydshaw@gmail.com>
2018-03-29 16:55:16 -0700

    input logic for handling group creation

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,4 +13,37 @@ class Company < ApplicationRecord
 
   has_many :groups
   has_many :employees
+
+  def setup_groups(employee_arr)
+    # O(n) clone our array elements so we dont mutate original
+    employees = employee_arr.map { |el| el }
+
+    group_qty = find_group_size(employee_arr)
+
+    # instantiate a 2D array as group buckets
+    result = Array.new(group_qty) { Array.new }
+    # O(n) placement of each employee into a group bucket
+    until employees.empty?
+      result.each do |arr|
+        arr << employees.shift
+      end
+    end
+    result
+  end
+
+  def find_group_size(arr)
+    length_to_check = is_prime?(arr.length) ? arr.length - 1 : arr.length
+    amount = 3
+    while amount <= length_to_check
+      if length_to_check % amount == 0
+        return amount
+      else
+        amount += 1
+      end
+    end
+  end
+
+  def is_prime?(num)
+    (1...num).select { |el| num % el == 0 }.reduce(:+) == 1
+  end
 end

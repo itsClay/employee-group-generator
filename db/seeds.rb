8eaedcc96a640c350ae2a7baf3970f3b570a79c4
Clay Shaw <Claydshaw@gmail.com>
2018-03-29 16:52:17 -0700

    input basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+user1 = User.create!(username: 'user', password: 'password')
+
+Company.destroy_all
+company1 = Company.create!(name: 'ACME')
+
+Employee.destroy_all
+employee1 = Employee.create!(first_name: 'John', last_name: 'Snow')
+employee2 = Employee.create!(first_name: 'Arya', last_name: 'Stark')
+employee3 = Employee.create!(first_name: 'The', last_name: 'Mountain')
+employee4 = Employee.create!(first_name: 'Ned', last_name: 'Stark')
+employee5 = Employee.create!(first_name: 'Tyrion', last_name: 'Lannister')
+employee6 = Employee.create!(first_name: 'Robert', last_name: 'Boratheon')
+employee7 = Employee.create!(first_name: 'Daenerys', last_name: 'Targaryen')
+
+Group.destroy_all
+group1 = Group.create!(company_id: Company.first.id)

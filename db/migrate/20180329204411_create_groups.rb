81234dc9f9b6b72b8a434b2c8fa0b66d1e5aeae4
Clay Shaw <Claydshaw@gmail.com>
2018-03-29 16:53:14 -0700

    add fields for group joins

--- db/migrate/20180329204411_create_groups.rb
+++ db/migrate/20180329204411_create_groups.rb
@@ -1,7 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :groups do |t|
-
+      t.integer :company_id
+      t.integer :user_id
+      
       t.timestamps
     end
   end
